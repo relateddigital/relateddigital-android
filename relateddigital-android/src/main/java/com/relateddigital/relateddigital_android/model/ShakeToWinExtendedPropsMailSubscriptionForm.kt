8172fbacb2dc6a42dbe70829dc3a0c8605ee0a81
FatihUtkuKara <fatihutkukara@gmail.com>
2022-12-16 14:49:09 +0300

    creating ShakeToWin model

---
+++ relateddigital-android/src/main/java/com/relateddigital/relateddigital_android/model/ShakeToWinExtendedPropsMailSubscriptionForm.kt
@@ -0,0 +1,5 @@
+package com.relateddigital.relateddigital_android.model
+import java.io.Serializable
+
+class ShakeToWinExtendedPropsMailSubscriptionForm : Serializable {
+}
---
+++ relateddigital-android/src/main/java/com/relateddigital/relateddigital_android/model/ShakeToWinMailSubscriptionForm.kt
@@ -0,0 +1,8 @@
+package com.relateddigital.relateddigital_android.model
+
+import com.google.gson.annotations.SerializedName
+import java.io.Serializable
+
+class ShakeToWinMailSubscriptionForm : Serializable {
+
+}
